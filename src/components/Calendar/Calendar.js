cb48024cf8d9beee1be76ab36cf3f86ce2ad0ad7
Naomi Yocum <naomiyocum24@gmail.com>
2023-06-08 15:43:01 -0600

    add from-to dates and highlight todays date

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import {meals, DAYS} from '../../utils';
+import {meals, DAYS, addDateBy, areDatesSame, getSunday} from '../../utils';
 import './Calendar.css';
 
 
 const Calendar = () => {
   return (
     <>
+    <FlexBox>
+      <p>from: {getSunday()?.toDateString()}</p>
+      <p>today: {new Date().toDateString()}</p>
+      <p>to: {addDateBy(getSunday(), 6).toDateString()}</p>
+    </FlexBox>
     <div className="wrapper">
       <HGrid className="main-hgrid" first={"30px"} cols={1}>
         <VGrid rows={3}>
@@ -16,10 +21,12 @@ const Calendar = () => {
         </VGrid>
         <HGrid className="days-grid" cols={7}>
           {
-            DAYS.map((day) => (
-            <span className="day-wrapper">
+            DAYS.map((day, index) => (
+            <DayWrapper isToday={
+              areDatesSame(new Date(), addDateBy(getSunday(), index))
+            }>
               <p className="each-day">{day}</p>
-            </span>
+            </DayWrapper>
             ))
           }
         </HGrid>
@@ -40,6 +47,18 @@ grid-template-rows: repeat(${({ rows }) => rows}, 1fr);
 &:first-child {
   margin-top: 15px;
 };
+`
+
+const DayWrapper = styled.span`
+border: 1px solid black;
+text-align: center;
+background: ${({ isToday }) => isToday ? '#b5d7a8': ''}
+`
+
+const FlexBox = styled.div`
+display: flex;
+justify-content: space-around;
+font-size: 0.8rem;
 `
 
 export default Calendar;
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,19 @@
 export const meals = ['Breakfast', 'Lunch', 'Dinner']
 export const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat']
+
+export const areDatesSame = (first, second) => {
+  return first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate()
+}
+
+export const addDateBy = (date, count) => {
+  const d = new Date(date);
+  return new Date(d.setDate(d.getDate() + count))
+}
+
+export const getSunday = () => {
+  const today = new Date();
+  const first = today.getDate() - today.getDay();
+  return new Date(today.setDate(first))
+}
