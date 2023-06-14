0d552a02ff1768893b4b97d56375af10112f3bbb
Naomi Yocum <naomiyocum24@gmail.com>
2023-06-14 15:15:56 -0600

    add conditional logic for user login

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../Header/Header'
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../Header/Header';
+import './Layout.css';
 
 function Layout() {
+  const loc = useLocation();
   return (
     <div>
       <Header />
+      {loc.pathname === "/" && <div className='coming-soon'>
+        <h1>WELCOME TO OUR PAGE</h1>
+        <h2>User Login coming soon...</h2>
+      </div>}
       <Outlet />
     </div>
   )
