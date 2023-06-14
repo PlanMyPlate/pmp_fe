ef036e086b6cee1c2ddeb952dcb1c83161105678
Naomi Yocum <naomiyocum24@gmail.com>
2023-06-14 14:36:16 -0600

    add functionality to switch between weeks

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import {meals, DAYS, MONTHS, addDateBy, areDatesSame, getSunday} from '../../utils';
 import './Calendar.css';
 
 
 const Calendar = () => {
+  const [sundayDate, setSundayDate] = useState(getSunday());
+  const nextWeek = () => setSundayDate(addDateBy(sundayDate, 7));
+  const prevWeek = () => setSundayDate(addDateBy(sundayDate, -7));
+  const orgWeek = () => setSundayDate(getSunday());
+
   return (
     <>
     <FlexBox>
-      <h2>{MONTHS[new Date().getUTCMonth()]} Plates</h2>
+      <button onClick={prevWeek}><ion-icon name="arrow-back-outline"></ion-icon></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <h2>{MONTHS[sundayDate.getUTCMonth()]} Plates</h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button onClick={nextWeek}><ion-icon name="arrow-forward-outline"></ion-icon></button>
     </FlexBox>
+    {sundayDate.getDate() !== getSunday().getDate() && <button className="today-button" onClick={orgWeek}>Today</button>}
     <div className="wrapper">
       <HGrid className="main-hgrid" first={"30px"} cols={1}>
         <VGrid rows={3}>
@@ -22,15 +30,19 @@ const Calendar = () => {
             DAYS.map((day, index) => (
             <DayWrapper 
             isToday={
-              areDatesSame(new Date(), addDateBy(getSunday(), index))
+              areDatesSame(new Date(), addDateBy(sundayDate, index))
             }>
-                <p className="each-day">{day} - {addDateBy(getSunday(), index).getUTCMonth() + 1}/{addDateBy(getSunday(), index).getUTCDate()}</p>
+                <p className="each-day">
+                  {day} - {addDateBy(sundayDate, index).getUTCMonth() + 1}/
+                  {addDateBy(sundayDate, index).getUTCDate()}
+                </p>
             </DayWrapper>
             ))
           }
         </HGrid>
       </HGrid>
     </div>
+    <p className="current-year">{sundayDate.getFullYear()}</p>
     </>
   );
 };
@@ -56,8 +68,12 @@ background: ${({ isToday }) => isToday ? '#b5d7a8': ''}
 
 const FlexBox = styled.div`
 display: flex;
-justify-content: space-around;
+justify-content: center;
 font-size: 0.8rem;
+
+button {
+  font-size: 1rem;
+}
 `
 
 export default Calendar;
