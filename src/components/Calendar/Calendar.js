bfb3a22479cf7001fff2206c5bfd70d48995d774
Naomi Yocum <naomiyocum24@gmail.com>
2023-06-21 15:03:31 -0600

    add bootstrap icons for calendar arrows

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -14,9 +14,13 @@ const Calendar = () => {
   return (
     <>
     <FlexBox>
-      <button onClick={prevWeek}><ion-icon name="arrow-back-outline"></ion-icon></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <h2>{MONTHS[sundayDate.getUTCMonth()]} Plates</h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <button onClick={nextWeek}><ion-icon name="arrow-forward-outline"></ion-icon></button>
+      <button type="button" class="btn btn-light" onClick={prevWeek}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+      </svg></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <h2 className="month-title">{MONTHS[sundayDate.getUTCMonth()]} Plates</h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button type="button" class="btn btn-light" onClick={nextWeek}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+      </svg></button>
     </FlexBox>
       {sundayDate.getDate() !== getSunday().getDate() && <div className="today-button"><Button variant="outline-info" size="sm" onClick={orgWeek}>Today</Button></div>}
     <div className="wrapper">
